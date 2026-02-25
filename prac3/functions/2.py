006853a9906d7d1ada013cc39592934dad9a1d31
kenesnurdaulet <nurd_kenes@kbtu.kz>
2026-02-26 02:41:17 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ prac3/functions/2.py
@@ -0,0 +1,19 @@
+def KBTU():
+  print("Hello from KBTU")
+
+KBTU()
+  
+def  Atyrau():
+  print("Hello from Atyrau")
+
+Atyrau()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def cout():
+  print("Hello")
+
+cout()
